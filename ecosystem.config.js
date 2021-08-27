9be777743f0a8ed52a322c4335aeb0294888fa9c
zhuyan <zhuyan9732@163.com>
2021-08-27 18:31:52 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       host: "42.193.155.28",
       ref: "origin/main",
       repo: "git@github.com:zhuyan9732/demo.git",
-      path: "/app",
+      path: "/app/demo",
       "post-deploy":
         "git reset --hard && git checkout main && git pull && npm i --production=false && pm2 startOrReload ecosystem.config.js", // -production=false 下载全量包
       env: {
