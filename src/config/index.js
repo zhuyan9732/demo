6bbcbb52c0117c19d6d223a706cff516a139f25e
zhuyan <zhuyan9732@163.com>
2021-08-28 11:28:45 +0800

    update

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,4 @@
 const MODE = import.meta.env.MODE; // 环境变量
 
-export const baseUrl = MODE == "development" ? "/api" : "http://42.193.155.28";
+export const baseUrl =
+  MODE == "development" ? "/api" : "http://42.193.155.28:7001/api";
